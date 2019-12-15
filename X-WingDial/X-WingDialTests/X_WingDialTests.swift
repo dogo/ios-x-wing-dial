661dc315a4981d37b08cafed3bef9e02248e1e24
Diogo Autilio <diautilio@gmail.com>
2019-12-15 02:56:00 -0300

    Init project

---
+++ X-WingDial/X-WingDialTests/X_WingDialTests.swift
@@ -0,0 +1,34 @@
+//
+//  X_WingDialTests.swift
+//  X-WingDialTests
+//
+//  Created by Diogo Autilio on 14/12/19.
+//  Copyright © 2019 Diogo Autilio. All rights reserved.
+//
+
+import XCTest
+@testable import X_WingDial
+
+class X_WingDialTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
