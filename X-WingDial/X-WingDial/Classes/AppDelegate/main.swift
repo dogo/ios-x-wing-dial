2b4f1b1148a4a0946a346af633583ab5ca7666ad
Diogo Autilio <diautilio@gmail.com>
2019-12-17 13:57:48 -0300

    Add main class

---
+++ X-WingDial/X-WingDial/Classes/AppDelegate/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  X-WingDial
+//
+//  Created by Diogo Autilio on 17/12/19.
+//  Copyright © 2019 Diogo Autilio. All rights reserved.
+//
+
+import UIKit
+
+let kIsRunningTests = NSClassFromString("XCTestCase") != nil
+let kAppDelegateClass = kIsRunningTests ? nil : NSStringFromClass(AppDelegate.self)
+UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, NSStringFromClass(UIApplication.self), kAppDelegateClass)
