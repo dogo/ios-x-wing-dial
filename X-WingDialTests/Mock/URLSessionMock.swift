2802ee7fb1d7f1e541e872881c18fef3946d2158
Diogo Autilio <diautilio@gmail.com>
2023-09-03 09:48:04 -0300

    Add HttpClientTests

---
+++ X-WingDialTests/Mock/URLSessionMock.swift
@@ -0,0 +1,67 @@
+//
+//  URLSessionMock.swift
+//  X-WingDialTests
+//
+//  Created by Diogo Autilio on 02/09/23.
+//  Copyright © 2023 Diogo Autilio. All rights reserved.
+//
+
+import Foundation
+
+final class URLSessionMock {
+
+    func build() -> URLSession {
+        let sessionConfiguration = URLSessionConfiguration.ephemeral
+        sessionConfiguration.protocolClasses = [URLProtocolMock.self]
+        return URLSession(configuration: sessionConfiguration)
+    }
+}
+
+final class URLProtocolMock: URLProtocol {
+
+    static var response: ((URLRequest) throws -> (HTTPResponse?))?
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+
+        do {
+            guard var urlRequest = try Self.response?(request) else {
+                return
+            }
+
+            if let response = urlRequest.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+
+            if let data = urlRequest.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+
+    override func stopLoading() {}
+}
+
+struct HTTPResponse {
+
+    var data: Data?
+    let statusCode: Int
+
+    lazy var response: HTTPURLResponse? = {
+        let response = HTTPURLResponse(url: URL(string: "http://base.url.com")!,
+                                       statusCode: statusCode,
+                                       httpVersion: nil,
+                                       headerFields: nil)
+        return response
+    }()
+}
---
+++ X-WingDialTests/Rest/HttpClientTests.swift
@@ -0,0 +1,122 @@
+//
+//  HttpClientTests.swift
+//  X-WingDialTests
+//
+//  Created by Diogo Autilio on 02/09/23.
+//  Copyright © 2023 Diogo Autilio. All rights reserved.
+//
+
+import Foundation
+import Nimble
+import Quick
+
+@testable import X_WingDial
+
+final class HttpClientTests: AsyncSpec {
+
+    override class func spec() {
+
+        describe("HttpClient") {
+            var sut: HttpClient!
+            var session: URLSession!
+            var request: URLRequest!
+
+            beforeEach {
+                session = URLSessionMock().build()
+                sut = HttpClient(session: session)
+                request = URLRequest(with: URL(string: "https://base.url.com")!)
+                request.httpMethod = HttpMethod.get.toString()
+            }
+
+            context("requesting with success") {
+
+                it("should decode with success") {
+                    URLProtocolMock.response = { _ in
+                        return HTTPResponse(data: "{ \"bar\": true }".data(using: .utf8),
+                                            statusCode: 200)
+                    }
+
+                    await expect { try? await sut.request(request, decode: Foo.self).bar } == true
+                }
+            }
+
+            context("requesting with failure") {
+
+                it("with response unsuccessful") {
+                    URLProtocolMock.response = { _ in
+                        return HTTPResponse(statusCode: 404)
+                    }
+
+                    await expect {
+                        try await sut.request(request, decode: Foo.self)
+                    }.to(throwError(APIError.responseUnsuccessful))
+                }
+
+                xit("with response json conversion failure") {
+                    URLProtocolMock.response = { _ -> HTTPResponse in
+                        return HTTPResponse(data: "{ \"id\": 3465 }".data(using: .utf8),
+                                            statusCode: 200)
+                    }
+
+                    await expect {
+                        try await sut.request(request, decode: Foo.self)
+                    }.to(throwError(APIError.jsonConversionFailure(domain: "j", description: "j")))
+                }
+
+                xit("with response invalid data") {
+                    URLProtocolMock.response = { _ in
+                        return HTTPResponse(statusCode: 200)
+                    }
+
+                    await expect {
+                        try await sut.request(request, decode: Foo.self)
+                    }.to(throwError(APIError.invalidData))
+                }
+
+                it("with response data corrupted") {
+                    URLProtocolMock.response = { _ in
+                        return HTTPResponse(statusCode: 200)
+                    }
+
+                    await expect {
+                        try await sut.request(request, decode: Foo.self)
+                    }.to(throwError(APIError.dataCorrupted(context: "The given data was not valid JSON.")))
+                }
+
+                it("with cancelled error") {
+//                    session.response = nil
+//                    session.error = NSError(domain: NSURLErrorDomain, code: NSURLErrorCancelled, userInfo: nil)
+                    URLProtocolMock.response = { _ in
+                        return HTTPResponse(statusCode: 200)
+                    }
+
+                    await expect {
+                        try await sut.request(request, decode: Foo.self)
+                    }.to(throwError(APIError.requestCancelled))
+                }
+
+                it("with server error reason") {
+//                    session.response = nil
+//                    session.error = NSError(domain: NSURLErrorDomain, code: NSURLErrorUnsupportedURL, userInfo: nil)
+//
+//                    sut.request(request) { (result: Result<Bool, APIError>) in
+//                        if case let .failure(error) = result {
+//                            expect(error.localizedDescription) == "Request failed with reason: The operation couldn’t be completed. (NSURLErrorDomain error -1002.)"
+//                        } else {
+//                            fail("Should be a failure result")
+//                        }
+//                    }
+                }
+            }
+
+            it("should cancel all requests") {
+                sut.cancelAllRequests()
+                // expect(session.tasksCancelled) == true
+            }
+        }
+    }
+}
+
+struct Foo: Decodable {
+    var bar: Bool
+}
